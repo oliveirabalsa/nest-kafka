3183470ba09fc93fc67f9b602807a104070876be
Leonardo <oliveirabalsa2@gmail.com>
2021-06-15 22:25:00 -0300

    feat: implement Kafka consumer

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @MessagePattern('Topic1') // Our topic name
+  getHello(@Payload() message) {
+    console.log(message.value);
+    return 'Hello World';
   }
 }
